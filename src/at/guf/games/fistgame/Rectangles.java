e7db28dffd40a7dd5ac904fa15c83f3e5667440a
Florian <florian.gugele@gmail.com>
2023-06-01 09:12:26 +0200

    Rectangles + ObjectGames erweitert

--- src/at/guf/games/fistgame/Rectangles.java
+++ src/at/guf/games/fistgame/Rectangles.java
@@ -1,4 +1,27 @@
 package at.guf.games.fistgame;
 
+import java.awt.*;
+
 public class Rectangles {
+
+    private enum DIRECTION {RIGHT,DOWN,LEFT,UP};
+    private float x;
+    private float y;
+    private float speed;
+
+    public Rectangles(float x, float y, float speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+
+    public void render (Graphics graphics){
+        graphics.drawRect(this.x,this.y,10,10 );
+    }
+    public void update (int delta){
+        this.x += (float)delta/speed;
+        if (this.x>600){
+            this.x=0;
+        }
+    }
 }
